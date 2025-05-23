cef52f475dcc6f959cae83f42616f5c4b44aa906
tomas_chang <tomas_chang@mayohr.com>
2025-05-23 16:26:42 +0800

    feature/ui:add frontend ui and data sync features

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./frontend/src"),
+      "shared/": path.resolve(__dirname, "./shared/"),
     },
   },
   envDir: "../",
